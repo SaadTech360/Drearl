36a940777e2d0673980d1a801d9d69616b8a1ce9
SaadTech360 <saadibrahimumar7@gmail.com>
2025-10-31 14:18:03 +0100

    fix some isues

--- front-end/eslint.config.mjs
+++ front-end/eslint.config.mjs
@@ -16,7 +16,9 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
     },
-
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
